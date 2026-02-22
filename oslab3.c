d6b4db204d11fbafb855e6893b3d8ba3cc005730
Aaryan181205 <kumararyan55269@gmail.com>
2026-02-22 12:54:05 +0530

    Os question of lab

---
+++ oslab3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n = 5;
+
+    int AT[5] = {0, 1, 2, 3, 4};
+    int BT[5] = {5, 7, 6, 2, 4};
+
+    int CT[5], TAT[5], WT[5];
+    CT[0] = AT[0] + BT[0];
+
+    for(int i = 1; i < n; i++) 
+    {
+        if(CT[i-1] < AT[i])
+            CT[i] = AT[i] + BT[i];
+        else
+            CT[i] = CT[i-1] + BT[i];
+    }
+
+    for(int i = 0; i < n; i++) 
+    {
+        TAT[i] = CT[i] - AT[i];
+        WT[i] = TAT[i] - BT[i];
+    }
+
+    float avgTAT = 0, avgWT = 0;
+
+    printf("\nProcess\tAT\tBT\tCT\tTAT\tWT\n");
+
+    for(int i = 0; i < n; i++)
+    {
+        avgTAT += TAT[i];
+        avgWT += WT[i];
+
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i, AT[i], BT[i], CT[i], TAT[i], WT[i]);
+    }
+
+    avgTAT /= n;
+    avgWT /= n;
+
+    printf("\nAverage Turnaround Time = %.2f", avgTAT);
+    printf("\nAverage Waiting Time = %.2f\n", avgWT);
+
+    return 0;
+}
